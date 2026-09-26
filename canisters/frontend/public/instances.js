d843b5c66e7a24582e30378515024099a98d05d0
Alexander Lorimer <aodlorimer@gmail.com>
2026-09-26 13:32:45 +0000

    Pin canonical backend hash in frontend registry

--- canisters/frontend/public/instances.js
+++ canisters/frontend/public/instances.js
@@ -1,5 +1,5 @@
 export const INSTANCES = Object.freeze([
-  Object.freeze({ id:'icp', name:'ICP', symbol:'ICP', alias:'X', status:'live', canonical:true, backendCanisterId:'eo6ei-gaaaa-aaaar-qchra-cai', observedLedgerCanisterId:'ryjl3-tyaaa-aaaaa-aaaba-cai', expectedBackendWasmSha256:null }),
+  Object.freeze({ id:'icp', name:'ICP', symbol:'ICP', alias:'X', status:'live', canonical:true, backendCanisterId:'eo6ei-gaaaa-aaaar-qchra-cai', observedLedgerCanisterId:'ryjl3-tyaaa-aaaaa-aaaba-cai', expectedBackendWasmSha256:'0f37ebed8655e27c2cebc0e5c9690216ab230daba10897181149a9a9b65c8187' }),
   Object.freeze({ id:'io', name:'IO', symbol:'IO', alias:'I', status:'planned', canonical:true, backendCanisterId:null, observedLedgerCanisterId:null, expectedBackendWasmSha256:null }),
 ]);
 export const defaultInstance = () => INSTANCES[0];
